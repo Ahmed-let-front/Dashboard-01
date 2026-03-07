52182431b79c1dc5836dfe8435fbfcdb2c6f40ef
Ahmed <letcosdgp@gmail.com>
2026-03-07 07:53:28 +0200

    change for others pages

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,22 @@
 import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
+import { resolve } from "path";
 export default defineConfig({
   plugins: [tailwindcss()],
   base: "/Dashboard-01",
   build: {
     chunkSizeWarningLimit: 2000,
     rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        settings: resolve(__dirname, "settings.html"),
+        profile: resolve(__dirname, "profile.html"),
+        projects: resolve(__dirname, "projects.html"),
+        courses: resolve(__dirname, "courses.html"),
+        friends: resolve(__dirname, "friends.html"),
+        files: resolve(__dirname, "files.html"),
+        plans: resolve(__dirname, "plans.html"),
+      },
       output: {
         assetFileNames: "assets/[name]-[hash][extname]",
         chunkFileNames: "assets/[name]-[hash].js",
@@ -19,4 +30,3 @@ export default defineConfig({
     },
   },
 });
-
